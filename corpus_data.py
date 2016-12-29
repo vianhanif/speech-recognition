c38a87521bf6e7e76972d3ad5323f29ad8753313
vianhanif <alvian524@gmail.com>
2016-12-29 23:12:44 +0700

    add corpus

--- corpus_data.py
+++ corpus_data.py
@@ -16,19 +16,64 @@ class CorpusData:
                     "have any name"
                 ],
                 [
-                    "I used to be called as %s" % (self.robot.get_name()),
-                    "You can call me %s" % (self.robot.get_name())
+                    "I am used to be called as %s" % (self.robot.get_name()),
+                    "You can call me %s" % (self.robot.get_name()),
+                    "I am identified as %s" % (self.robot.get_name()),
+                    "though my name could change overtime, but you can call me %s for now" % (self.robot.get_name()),
+                    "as you can see the label, my name is %s" % (self.robot.get_name())
                 ],
             ],
             # acknowledge user name
             [
                 [
                     "My name is",
-                    "My friends used to call me"
+                    "used to call me",
+                    "call me",
                 ],
                 [
                     "Nice to meet you",
-                    "Hi, there"
+                    "Hi, there",
+                    "Happy to see you",
+                ]
+            ],
+            # acknoledge user age
+            [
+                [
+                    "years old",
+                ],
+                [
+                    "I hope you will stay healthy",
+                    "no matter how old, you'll still growing for a better person",
+                    "Always happy to see a healthy person at such age",
+                    "I expect I can get close with such person now",
+                ]
+            ],
+            [
+                [
+                    "living in",
+                    "live in",
+                ],
+                [
+                    "I hope you happiness by living there",
+                    "I wish I can see your place",
+                    "your home must be looking really nice",
+                    "I can never compare mine with such a place",
+                ]
+            ],
+            # acknowledge user job
+            [
+                [
+                    "working as",
+                    "work in",
+                    "job is",
+                ],
+                [
+                    "Such an enjoyable job",
+                    "Such responsibility must have made you proudful",
+                    "your family must be proud of you",
+                    "You must be busy",
+                    "if I may, I am proud of you",
+                    "Such an interesting job",
                 ]
             ],
         ]
